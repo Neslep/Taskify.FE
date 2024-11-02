e11376f6b3085371b23c840f2ee4f78bf2824326
Neslep <kbtorgan@gmail.com>
2024-11-02 23:14:47 +0700

    test say hi

--- src/sections/overview/view/overview-analytics-view.tsx
+++ src/sections/overview/view/overview-analytics-view.tsx
@@ -26,7 +26,7 @@ export function OverviewAnalyticsView() {
   return (
     <DashboardContent maxWidth="xl">
       <Typography variant="h4" sx={{ mb: { xs: 3, md: 5 } }}>
-        Hi, Welcome back 👋
+        Hi, Welcome back EXE FPT Group 4 👋
       </Typography>
 
       <Grid container spacing={3}>
