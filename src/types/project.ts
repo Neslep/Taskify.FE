08a46951becc3c0e8c063a0a64406cfbe39f49ac
Quốc Huy <qhuy14775@gmail.com>
2025-03-10 22:41:30 +0700

    Project UI Logic

---
+++ src/types/project.ts
@@ -0,0 +1,60 @@
+import {PlanType, ProjectRoles, ProjectStatus} from "./enum";
+
+export interface ProjectType {
+    id: string;
+    projectName: string;
+    description: string;
+    projectStatus: ProjectStatus | string;
+    ownerId?: number;
+    owner?: UserType;
+    userProjects: UserProjectType[];
+    tasks: TaskType[];
+    kanbans: KanbanType[];
+    todolists: TodolistType[];
+}
+
+export interface UserType {
+    id: number;
+    name: string;
+    email: string;
+    plans: PlanType | string;
+}
+
+export interface UserProjectType {
+    id: number;
+    userId: number;
+    projectId: string;
+    roleInProject: ProjectRoles | string;
+    // Add other properties as needed
+}
+
+export interface TaskType {
+    id: string;
+    name: string;
+    // Add other properties as needed
+}
+
+export interface KanbanType {
+    id: string;
+    name: string;
+    // Add other properties as needed
+}
+
+export interface TodolistType {
+    id: string;
+    name: string;
+    // Add other properties as needed
+}
+
+export type ProjectProps = {
+    id: string;
+    name: string;
+    description: string;
+    status: ProjectStatus; // Now it's of type ProjectStatus
+};
+
+export type ProjectTableRowProps = {
+    row: ProjectProps;
+    selected: boolean;
+    onSelectRow: () => void;
+};
